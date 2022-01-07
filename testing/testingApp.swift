e9ce17efee8a2f0e79bfe6c9db03ea932f5720c9
Nisan Coşkun <info@nisancoskun.com>
2022-01-07 10:40:52 +0300

    Initial Commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Nisan Coskun on 7.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
